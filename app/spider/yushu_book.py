c815c23566a099acbb143df04421dd24c191c2bc
lmh_mac <2089914858@qq.com>
2019-03-27 22:53:24 +0800

    update

--- app/spider/yushu_book.py
+++ app/spider/yushu_book.py
@@ -5,18 +5,28 @@ class YuShuBook:
     isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
     keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
 
-    @classmethod
-    def searchByIsbn(cls, isbn):
-        url = cls.isbn_url.format(isbn)
-        response =  Http.get(url)
-        return response
+    def __init__(self):
+        self.total = 0
+        self.books = []
 
-    @classmethod
-    def searchByKeyword(cls, keyword, page=1):
-        url = cls.keyword_url.format(keyword, current_app.config['PER_PAGE'], cls.calculate_start(page))
+    def searchByIsbn(self, isbn):
+        url = self.isbn_url.format(isbn)
         response =  Http.get(url)
-        return response
+        self.__fill_single(response)
 
-    @staticmethod
-    def calculate_start(page):
+    def searchByKeyword(self , keyword, page=1):
+        url = self.keyword_url.format(keyword, current_app.config['PER_PAGE'], self.calculate_start(page))
+        response =  Http.get(url)
+        self.__fill_collection(response)
+
+    def __fill_single(self, data):
+        if data:
+            self.total = 1
+            self.books.append(data)
+    
+    def __fill_collection(self, data):
+        self.total = data['total']
+        self.books = data['books'] 
+
+    def calculate_start(self, page):
         return (page - 1) * current_app.config['PER_PAGE']
--- app/web/book.py
+++ app/web/book.py
@@ -1,8 +1,9 @@
-from flask import jsonify, request
+from flask import jsonify, request, render_template
+import json
 from app.forms.book import SearchForm
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
-from app.view_models.book import BookViewModel
+from app.view_models.book import BookCollection
 from . import web 
 
 @web.route('/book/search')
@@ -13,18 +14,27 @@ def search():
         9787070511209
     '''
     form = SearchForm(request.args)
+    books = BookCollection() 
     if form.validate():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
+        yushu_book = YuShuBook()
         if isbn_or_key == 'isbn':
-            result = YuShuBook.searchByIsbn(q)
-            result = BookViewModel.package_single(result, q)
+            yushu_book.searchByIsbn(q)
         else:
-            result = YuShuBook.searchByKeyword(q, page)
-            result = BookViewModel.package_collection(result, q)
+            yushu_book.searchByKeyword(q, page)
+        books.fill(yushu_book, q)
         # print(result)
-        return jsonify(result)
+        return json.dumps(books, default=lambda o: o.__dict__, ensure_ascii=False)
     else:
         return jsonify(form.errors)
     # return json.dumps(result), 200, {'content-type':'application/json'}
+
+@web.route('/test')
+def test():
+    test = {
+        'English': 'Hello World',
+        'Chinese': '你好 世界'
+    }
+    return render_template('test.html', data = test)
